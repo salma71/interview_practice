c205160c489eb9a8f59971b37bb490c92b6f39c7
Salma Elshahawy <salmaelshahawy@salmas-mbp-2.myfiosgateway.com>
2020-12-28 14:05:17 -0500

    replace and remove

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -6,8 +6,30 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def replace_and_remove(size: int, s: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    # remove the excess letter and count the number of a's
+    b_index , a_count = 0, 0
+    for i in range(size):
+        if s[i] != 'b':
+            s[b_index] = s[i]
+            b_index += 1
+        if s[i] == 'a':
+            a_count += 1
+    
+    # backward elimination replace a with dd
+    curr_index = b_index - 1
+    b_index += a_count - 1
+    final_size = b_index + 1
+    
+    while curr_index >= 0:
+        if s[curr_index] == 'a':
+            s[b_index - 1:b_index + 1] = 'dd'
+            b_index -= 2
+        else:
+            s[b_index] = s[curr_index]
+            b_index -= 1
+        curr_index -= 1
+    return final_size
+    
 
 
 @enable_executor_hook
